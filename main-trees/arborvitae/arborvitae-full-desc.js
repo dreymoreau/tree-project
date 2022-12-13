ad12f04ac2a1d9eff007d8bd2d3680158b6bb3ca
dreymotoh <striclyspoken@gmail.com>
2022-12-13 09:19:32 -0500

    changes!

---
+++ main-trees/arborvitae/arborvitae-full-desc.js
@@ -0,0 +1,5 @@
+document.querySelector('#buttonOne').addEventListener('click', openArborvitaePage)
+
+function openArborvitaePage() {
+    location.href=("../main-trees/arborvitae/arborvitae-full-desc.html")
+}
