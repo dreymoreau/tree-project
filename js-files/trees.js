29eafa67fa6aac139aca51b56408405cd350e6c4
dreymotoh <striclyspoken@gmail.com>
2022-12-16 12:41:42 -0500

    changes!

---
+++ js-files/trees.js
@@ -0,0 +1,29 @@
+document.querySelector('#buttonOne').addEventListener('click', openArborvitaePage)
+
+function openArborvitaePage() {
+    location.href=("../main-trees/arborvitae/arborvitae-full-desc.html")
+}
+
+document.querySelector('#buttonTwo').addEventListener('click', openHoneyLocusPage)
+
+function openHoneyLocusPage() {
+    location.href=("../main-trees/honey-locus/honey-locus-full-desc.html")
+}
+
+document.querySelector('#buttonThree').addEventListener('click', openSassafrassPage)
+
+function openSassafrassPage() {
+    location.href=("../main-trees/sassafrass/sassafrass-full-desc.html")
+}
+
+document.querySelector('#buttonFour').addEventListener('click', openBlackOakPage)
+
+function openBlackOakPage() {
+    location.href=("../main-trees/black-oak/black-oak-full-desc.html")
+}
+
+document.querySelector('#buttonFive').addEventListener('click', openRedMaplePage)
+
+function openRedMaplePage() {
+    location.href=("../main-trees/red-maple/red-maple-full-desc.html")
+}
--- main-trees/arborvitae/arborvitae-full-desc.js
+++
@@ -1,5 +0,0 @@
-document.querySelector('#buttonOne').addEventListener('click', openArborvitaePage)
-
-function openArborvitaePage() {
-    location.href=("../main-trees/arborvitae/arborvitae-full-desc.html")
-}
