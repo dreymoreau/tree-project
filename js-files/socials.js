09a19ac2592ed83d49ae4464139818aa27566eec
dreymotoh <striclyspoken@gmail.com>
2023-03-15 08:55:21 -0400

    removed clock from all pages instead of index.html

--- js-files/socials.js
+++
@@ -1,5 +0,0 @@
-setInterval(function () {
-    const currentDate = new Date();
-    document.querySelector("#curTime").innerText = currentDate.getHours() + ":" + 
-    (currentDate.getMinutes().toString().padStart(2, "0")) + ":" + (currentDate.getSeconds().toString().padStart(2, "0"));
-  }, 1000);
--- js-files/trees.js
+++ js-files/trees.js
@@ -27,9 +27,3 @@ document.querySelector('#buttonFive').addEventListener('click', openRedMaplePage
 function openRedMaplePage() {
     location.href=("../main-trees/red-maple/red-maple-full-desc.html")
 }
-
-setInterval(function () {
-    const currentDate = new Date();
-    document.querySelector("#curTime").innerText = currentDate.getHours() + ":" + 
-    (currentDate.getMinutes().toString().padStart(2, "0")) + ":" + (currentDate.getSeconds().toString().padStart(2, "0"));
-  }, 1000);
